1cb88f83a4aa485a2ca1641019d92dd1f76c10a5
Martin Meixger <mail@meixger.com>
2023-01-31 17:19:44 +0100

    POC devProxy & myFetch

---
+++ composables/myFetch.ts
@@ -0,0 +1,105 @@
+import { useRequestEvent } from "#app";
+import { appendHeader } from "h3";
+
+export default async function useMyFetch<T>(path: string) {
+  if (process.server) {
+    path = "http://localhost:3100" + path;
+    console.log("useMyFetch agument ssr path to", path);
+  }
+
+  const headers = useRequestHeaders(["cookie"]) as HeadersInit;
+  const event = useRequestEvent();
+  return await useAsyncData(() => forwardCookies());
+
+  async function forwardCookies() {
+    const res = await $fetch.raw<T>(path, { headers });
+
+    const cookies = splitCookiesString(res.headers.get("set-cookie") || "");
+    for (const cookie of cookies) {
+      appendHeader(event, "set-cookie", cookie);
+    }
+    return res._data ?? null;
+  }
+}
+
+/*
+  Set-Cookie header field-values are sometimes comma joined in one string. This splits them without choking on commas
+  that are within a single set-cookie field-value, such as in the Expires portion.
+  This is uncommon, but explicitly allowed - see https://tools.ietf.org/html/rfc2616#section-4.2
+  Node.js does this for every header *except* set-cookie - see https://github.com/nodejs/node/blob/d5e363b77ebaf1caf67cd7528224b651c86815c1/lib/_http_incoming.js#L128
+  React Native's fetch does this for *every* header, including set-cookie.
+  Based on: https://github.com/google/j2objc/commit/16820fdbc8f76ca0c33472810ce0cb03d20efe25
+  Credits to: https://github.com/tomball for original and https://github.com/chrusart for JavaScript implementation
+*/
+function splitCookiesString(cookiesString: string) {
+  if (Array.isArray(cookiesString)) {
+    return cookiesString;
+  }
+  if (typeof cookiesString !== "string") {
+    return [];
+  }
+
+  var cookiesStrings = [];
+  var pos = 0;
+  var start;
+  var ch;
+  var lastComma;
+  var nextStart;
+  var cookiesSeparatorFound;
+
+  function skipWhitespace() {
+    while (pos < cookiesString.length && /\s/.test(cookiesString.charAt(pos))) {
+      pos += 1;
+    }
+    return pos < cookiesString.length;
+  }
+
+  function notSpecialChar() {
+    ch = cookiesString.charAt(pos);
+
+    return ch !== "=" && ch !== ";" && ch !== ",";
+  }
+
+  while (pos < cookiesString.length) {
+    start = pos;
+    cookiesSeparatorFound = false;
+
+    while (skipWhitespace()) {
+      ch = cookiesString.charAt(pos);
+      if (ch === ",") {
+        // ',' is a cookie separator if we have later first '=', not ';' or ','
+        lastComma = pos;
+        pos += 1;
+
+        skipWhitespace();
+        nextStart = pos;
+
+        while (pos < cookiesString.length && notSpecialChar()) {
+          pos += 1;
+        }
+
+        // currently special character
+        if (pos < cookiesString.length && cookiesString.charAt(pos) === "=") {
+          // we found cookies separator
+          cookiesSeparatorFound = true;
+          // pos is inside the next cookie, so back up and return it.
+          pos = nextStart;
+          cookiesStrings.push(cookiesString.substring(start, lastComma));
+          start = pos;
+        } else {
+          // in param ',' or param separator ';',
+          // we continue from that comma
+          pos = lastComma + 1;
+        }
+      } else {
+        pos += 1;
+      }
+    }
+
+    if (!cookiesSeparatorFound || pos >= cookiesString.length) {
+      cookiesStrings.push(cookiesString.substring(start, cookiesString.length));
+    }
+  }
+
+  return cookiesStrings;
+}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  runtimeConfig: {
+    // The private keys which are only available within server-side
+    apiSecret: "123",
+    // Keys within public, will be also exposed to the client-side
+    public: {
+      dotnetport: process.env.DOTNET_PORT || "5000",
+      nuxtport: process.env.NUXT_PORT || "3000",
+    },
+  },
+  nitro: {
+    devProxy: {
+      "/api/node": {
+        target: "http://localhost:3100/api/node",
+        headers: { "x-devproxy": "yes" },
+      },
+      "/proxy/setcookies": {
+        target: "http://localhost:3000/api/setcookies",
+        headers: { "x-devproxy": "yes" },
+      },
+      "/api/dotnet/": {
+        target: "http://localhost:5000/api/dotnet/",
+        headers: { "x-devproxy": "yes" },
+      },
+    },
+  },
+});
